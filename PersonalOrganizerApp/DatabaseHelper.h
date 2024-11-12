422b9a20c1e29facc914081e534e692d3b552da9
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2024-11-12 15:38:14 +0530

    added income/expenses functionality with db

--- PersonalOrganizerApp/DatabaseHelper.h
+++ PersonalOrganizerApp/DatabaseHelper.h
@@ -1,17 +1,17 @@
 #pragma once
-//#include <iostream>
-//#include <string>
-//#include <msclr/marshal_cppstd.h>
-//#using <System.Data.dll>
+#using <System.Data.dll>
 
 using namespace System;
+using namespace System::Data;
 using namespace System::Data::SqlClient;
+using namespace System::Windows::Forms;
 
 public ref class DatabaseHelper {
 private:
     static DatabaseHelper^ instance = nullptr;
     SqlConnection^ connection;
 
+    // Private constructor to prevent instantiation from outside
     DatabaseHelper() {
         String^ connectionString = "Data Source=DESKTOP-MDO4CSL\\sqlexpress;Initial Catalog=personalOrganizerDB;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
         connection = gcnew SqlConnection(connectionString);
@@ -25,6 +25,7 @@ public:
         return instance;
     }
 
+    // Open a connection to the database
     bool OpenConnection() {
         if (connection->State != System::Data::ConnectionState::Open) {
             connection->Open();
@@ -32,13 +33,30 @@ public:
         return connection->State == System::Data::ConnectionState::Open;
     }
 
+    // Close the connection to the database
     void CloseConnection() {
         if (connection->State != System::Data::ConnectionState::Closed) {
             connection->Close();
         }
     }
 
+    // Return the SqlConnection object
     SqlConnection^ GetConnection() {
         return connection;
+    }
+
+    // Method to execute a query and return a DataTable
+    DataTable^ ExecuteQuery(String^ query) {
+        SqlDataAdapter^ dataAdapter = gcnew SqlDataAdapter(query, connection);
+        DataTable^ dataTable = gcnew DataTable();  // Ensure that this is correctly declared
+        try {
+            if (OpenConnection()) {
+                dataAdapter->Fill(dataTable);  // This will fill the DataTable with results
+            }
+        }
+        catch (Exception^ e) {
+            MessageBox::Show("Error executing query: " + e->Message);
+        }
+        return dataTable;  // Return the filled DataTable
     }
 };
