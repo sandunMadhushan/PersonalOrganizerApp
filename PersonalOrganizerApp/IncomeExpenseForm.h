e698416bfb61bf883cff143188712b94aae2a775
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2024-11-12 14:04:07 +0530

    designed income/expenses UI

--- PersonalOrganizerApp/IncomeExpenseForm.h
+++ PersonalOrganizerApp/IncomeExpenseForm.h
@@ -35,6 +35,43 @@ namespace PersonalOrganizerApp {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
+
+
+
+
+
+	private: System::Windows::Forms::TabControl^ tabControl1;
+	private: System::Windows::Forms::TabPage^ tabPage2;
+
+	private: System::Windows::Forms::TabPage^ tabPage1;
+
+
+	private: System::Windows::Forms::GroupBox^ groupBox2;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::DateTimePicker^ dateTimePicker1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::ComboBox^ comboBox2;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::Label^ label9;
+	private: System::Windows::Forms::Label^ label10;
+	private: System::Windows::Forms::DateTimePicker^ dateTimePicker2;
+	private: System::Windows::Forms::Label^ label11;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+
 	protected:
 
 	private:
@@ -52,6 +89,40 @@ namespace PersonalOrganizerApp {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(IncomeExpenseForm::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->dateTimePicker2 = (gcnew System::Windows::Forms::DateTimePicker());
+			this->label11 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->tabControl1->SuspendLayout();
+			this->tabPage1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
+			this->tabPage2->SuspendLayout();
+			this->groupBox1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -59,18 +130,309 @@ namespace PersonalOrganizerApp {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(466, 45);
+			this->label1->Location = System::Drawing::Point(432, 56);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(217, 29);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Income / Expenses";
 			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->ImeMode = System::Windows::Forms::ImeMode::NoControl;
+			this->tabControl1->Location = System::Drawing::Point(142, 138);
+			this->tabControl1->Multiline = true;
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(804, 639);
+			this->tabControl1->TabIndex = 2;
+			this->tabControl1->SelectedIndexChanged += gcnew System::EventHandler(this, &IncomeExpenseForm::tabControl1_SelectedIndexChanged);
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Controls->Add(this->groupBox2);
+			this->tabPage1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->tabPage1->Location = System::Drawing::Point(4, 25);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(6);
+			this->tabPage1->Size = System::Drawing::Size(796, 610);
+			this->tabPage1->TabIndex = 1;
+			this->tabPage1->Text = L"Income";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->button3);
+			this->groupBox2->Controls->Add(this->button1);
+			this->groupBox2->Controls->Add(this->textBox2);
+			this->groupBox2->Controls->Add(this->comboBox1);
+			this->groupBox2->Controls->Add(this->textBox1);
+			this->groupBox2->Controls->Add(this->label7);
+			this->groupBox2->Controls->Add(this->label6);
+			this->groupBox2->Controls->Add(this->label5);
+			this->groupBox2->Controls->Add(this->label4);
+			this->groupBox2->Controls->Add(this->dateTimePicker1);
+			this->groupBox2->Controls->Add(this->label3);
+			this->groupBox2->Location = System::Drawing::Point(175, 87);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(431, 412);
+			this->groupBox2->TabIndex = 2;
+			this->groupBox2->TabStop = false;
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::SkyBlue;
+			this->button3->Location = System::Drawing::Point(88, 344);
+			this->button3->Name = L"button3";
+			this->button3->Padding = System::Windows::Forms::Padding(5);
+			this->button3->Size = System::Drawing::Size(90, 34);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"CLEAR";
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::SkyBlue;
+			this->button1->Location = System::Drawing::Point(240, 344);
+			this->button1->Name = L"button1";
+			this->button1->Padding = System::Windows::Forms::Padding(5);
+			this->button1->Size = System::Drawing::Size(90, 34);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"ADD";
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(165, 281);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(200, 22);
+			this->textBox2->TabIndex = 5;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) {
+				L"job", L"allowance from parents", L"scholarships",
+					L"student aid"
+			});
+			this->comboBox1->Location = System::Drawing::Point(165, 167);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(200, 24);
+			this->comboBox1->TabIndex = 4;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(165, 227);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(200, 22);
+			this->textBox1->TabIndex = 3;
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(49, 288);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(52, 16);
+			this->label7->TabIndex = 2;
+			this->label7->Text = L"Amount";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(49, 170);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(50, 16);
+			this->label6->TabIndex = 2;
+			this->label6->Text = L"Source";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(49, 227);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(75, 16);
+			this->label5->TabIndex = 2;
+			this->label5->Text = L"Description";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(49, 109);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(36, 16);
+			this->label4->TabIndex = 2;
+			this->label4->Text = L"Date";
+			// 
+			// dateTimePicker1
+			// 
+			this->dateTimePicker1->Location = System::Drawing::Point(165, 109);
+			this->dateTimePicker1->Name = L"dateTimePicker1";
+			this->dateTimePicker1->Size = System::Drawing::Size(200, 22);
+			this->dateTimePicker1->TabIndex = 1;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(162, 45);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(82, 25);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"Income";
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Controls->Add(this->groupBox1);
+			this->tabPage2->Cursor = System::Windows::Forms::Cursors::Default;
+			this->tabPage2->Location = System::Drawing::Point(4, 25);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(6);
+			this->tabPage2->Size = System::Drawing::Size(796, 610);
+			this->tabPage2->TabIndex = 0;
+			this->tabPage2->Text = L"Expenses";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->button4);
+			this->groupBox1->Controls->Add(this->button2);
+			this->groupBox1->Controls->Add(this->textBox3);
+			this->groupBox1->Controls->Add(this->comboBox2);
+			this->groupBox1->Controls->Add(this->textBox4);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->label8);
+			this->groupBox1->Controls->Add(this->label9);
+			this->groupBox1->Controls->Add(this->label10);
+			this->groupBox1->Controls->Add(this->dateTimePicker2);
+			this->groupBox1->Controls->Add(this->label11);
+			this->groupBox1->Location = System::Drawing::Point(175, 87);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(431, 412);
+			this->groupBox1->TabIndex = 3;
+			this->groupBox1->TabStop = false;
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::Color::SkyBlue;
+			this->button4->Location = System::Drawing::Point(88, 344);
+			this->button4->Name = L"button4";
+			this->button4->Padding = System::Windows::Forms::Padding(5);
+			this->button4->Size = System::Drawing::Size(90, 34);
+			this->button4->TabIndex = 8;
+			this->button4->Text = L"CLEAR";
+			this->button4->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::SkyBlue;
+			this->button2->Location = System::Drawing::Point(240, 344);
+			this->button2->Name = L"button2";
+			this->button2->Padding = System::Windows::Forms::Padding(5);
+			this->button2->Size = System::Drawing::Size(90, 34);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"ADD";
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(165, 281);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(200, 22);
+			this->textBox3->TabIndex = 5;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(6) {
+				L"food", L"entertainment", L"traveling", L"clothing",
+					L"education equipment", L"university fees"
+			});
+			this->comboBox2->Location = System::Drawing::Point(165, 226);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(200, 24);
+			this->comboBox2->TabIndex = 4;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(165, 168);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(200, 22);
+			this->textBox4->TabIndex = 3;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(49, 288);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(52, 16);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Amount";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(49, 229);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(62, 16);
+			this->label8->TabIndex = 2;
+			this->label8->Text = L"Category";
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(49, 171);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(75, 16);
+			this->label9->TabIndex = 2;
+			this->label9->Text = L"Description";
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Location = System::Drawing::Point(49, 109);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(36, 16);
+			this->label10->TabIndex = 2;
+			this->label10->Text = L"Date";
+			// 
+			// dateTimePicker2
+			// 
+			this->dateTimePicker2->Location = System::Drawing::Point(165, 109);
+			this->dateTimePicker2->Name = L"dateTimePicker2";
+			this->dateTimePicker2->Size = System::Drawing::Size(200, 22);
+			this->dateTimePicker2->TabIndex = 1;
+			// 
+			// label11
+			// 
+			this->label11->AutoSize = true;
+			this->label11->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label11->Location = System::Drawing::Point(162, 45);
+			this->label11->Name = L"label11";
+			this->label11->Size = System::Drawing::Size(107, 25);
+			this->label11->TabIndex = 0;
+			this->label11->Text = L"Expenses";
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(998, 29);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(126, 125);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 1;
+			this->pictureBox2->TabStop = false;
+			// 
 			// IncomeExpenseForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1146, 667);
+			this->ClientSize = System::Drawing::Size(1146, 859);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->tabControl1);
 			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
@@ -78,10 +440,35 @@ namespace PersonalOrganizerApp {
 			this->Name = L"IncomeExpenseForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Income / Expenses";
+			this->tabControl1->ResumeLayout(false);
+			this->tabPage1->ResumeLayout(false);
+			this->groupBox2->ResumeLayout(false);
+			this->groupBox2->PerformLayout();
+			this->tabPage2->ResumeLayout(false);
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+
+private: System::Void tabControl1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	// Check if switching to tabPage1
+	if (tabControl1->SelectedTab == tabPage1) {
+		// Clear controls in tabPage1 (Income)
+		textBox1->Clear(); // Clear any text boxes for Income
+		// Add other necessary controls for tabPage1 if needed
+	}
+	// Check if switching to tabPage2
+	else if (tabControl1->SelectedTab == tabPage2) {
+		// Clear controls in tabPage2 (Expenses)
+		dateTimePicker1->Value = DateTime::Now; // Reset date to today
+		textBox1->Clear(); // Clear Description
+		textBox2->Clear(); // Clear Amount
+		comboBox1->SelectedIndex = -1; // Reset combo box selection
+	}
+}
+};
 }
